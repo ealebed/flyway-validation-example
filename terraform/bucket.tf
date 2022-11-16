3578c81089562f4b4f8d5f44fbc7827c0655feeb
Yevhen Lebid <ylebi@softserveinc.com>
2022-11-16 20:13:00 +0200

    Set 'force_destroy' on bucket and on SQL 'deletion_protection' to false

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "default" {
   name          = "${var.gcp_project}-flyway-tfstate"
-  force_destroy = true
+  force_destroy = false
   location      = var.gcp_region
   storage_class = "STANDARD"
   versioning {
