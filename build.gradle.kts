ec5d1bf55f21e2477a51873f39a0924904e2c151
Yevhen Lebid <ylebi@softserveinc.com>
2023-11-13 10:17:52 +0200

    add dependency flyway-postgres

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,14 @@ repositories {
 }
 
 dependencies {
+    val flywayVersion = "10.0.0"
     val testcontainersVersion = "1.19.1"
     val jupiterVersion = "5.10.1"
 
     testImplementation("org.junit.jupiter:junit-jupiter:${jupiterVersion}")
     testImplementation("org.junit.jupiter:junit-jupiter-params:${jupiterVersion}")
-    testImplementation("org.flywaydb:flyway-core:10.0.0")
+    testImplementation("org.flywaydb:flyway-core:${flywayVersion}")
+    testImplementation("org.flywaydb:flyway-database-postgresql:${flywayVersion}")
     testImplementation("org.postgresql:postgresql:42.6.0")
     testImplementation("org.slf4j:slf4j-simple:2.0.9")
     testImplementation("org.testcontainers:junit-jupiter:${testcontainersVersion}")
